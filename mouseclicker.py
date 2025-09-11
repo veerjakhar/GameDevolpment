9cfca366a473842747a00dd1d74d2b9f9be513e1
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-09-10 18:58:08 -0700

    Add files via upload

---
+++ mouseclicker.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+#globsl varables
+screen = pygame.display.set_mode([500, 500])
+red = (255, 0 ,0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+yellow = (255, 255, 0)
